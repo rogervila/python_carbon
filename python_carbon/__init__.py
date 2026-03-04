284a0aefe241a4b0b4dd5de0e83ab2f821d9fe3a
Roger Vilà <rogervila@me.com>
2026-03-04 14:34:47 +0100

    fix: diffIn methods (#31)

--- python_carbon/__init__.py
+++ python_carbon/__init__.py
@@ -441,19 +441,19 @@ class Carbon:
         return method(carbon)
 
     def diffInMicroseconds(self, carbon: 'Carbon') -> int:
-        return (self._date - carbon.toDatetime()).microseconds
+        return int(round((self._date - carbon.toDatetime()).total_seconds() * 1_000_000))
 
-    def diffInSeconds(self, carbon: 'Carbon') -> int:
-        return (self._date - carbon.toDatetime()).seconds
+    def diffInSeconds(self, carbon: 'Carbon') -> float:
+        return (self._date - carbon.toDatetime()).total_seconds()
 
-    def diffInMinutes(self, carbon: 'Carbon') -> int:
+    def diffInMinutes(self, carbon: 'Carbon') -> float:
         return self.diffInSeconds(carbon) / 60
 
-    def diffInHours(self, carbon: 'Carbon') -> int:
-        return self.diffInMinutes(carbon) / 60
+    def diffInHours(self, carbon: 'Carbon') -> float:
+        return self.diffInSeconds(carbon) / 3600
 
-    def diffInDays(self, carbon: 'Carbon') -> int:
-        return (self._date - carbon.toDatetime()).days
+    def diffInDays(self, carbon: 'Carbon') -> float:
+        return self.diffInSeconds(carbon) / 86400
 
     def diffInWeeks(self, carbon: 'Carbon') -> float:
         return self.diffInDays(carbon) / 7
